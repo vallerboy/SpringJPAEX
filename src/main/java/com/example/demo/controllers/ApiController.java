a2ebfd40046874cdb9463f83564fcbab3cfd52b6
Oskar Pol <oskarbank@gmail.com>
2017-06-05 21:01:46 +0200

    WebServices

--- src/main/java/com/example/demo/controllers/ApiController.java
+++ src/main/java/com/example/demo/controllers/ApiController.java
@@ -7,10 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -36,26 +33,36 @@ public class ApiController {
         return new ResponseEntity(users, HttpStatus.OK);
     }
 
+    @RequestMapping(value = "/api/user/{username}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getUser(@RequestHeader("Access-Password")String key, @PathVariable("username") String username){
+        if(!key.equals("akademiakodujestfajna")){
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        Optional<User> user = userRepository.findByUsername(username);
+        if(!user.isPresent()){
+            return new ResponseEntity("User no exist", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(user.get(), HttpStatus.OK);
+    }
 
-    @RequestMapping(value = "/api/user/{username}",
-            method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-
-    public ResponseEntity getUser(@RequestHeader("Access-Password") String key,
-                                  @PathVariable("username") String username){
-
-        if(!key.equals("akademiakodujestfajna")) {
+    @RequestMapping(value =  "/api/user", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity editUser(@RequestBody User user, @RequestHeader("Access-Password")String key) {
+        if(!key.equals("akademiakodujestfajna")){
             return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
 
-        Optional<User> user = userRepository.findByUsername(username);
-        if(!user.isPresent()) {
-            return new ResponseEntity("User no exist", HttpStatus.NOT_FOUND);
+        if(user == null) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
 
-        return new ResponseEntity(user.get(), HttpStatus.OK);
+        User userLocal = userRepository.findOne(user.getId());
+        if(userLocal == null){
+            return new ResponseEntity("User no exist", HttpStatus.BAD_REQUEST);
+        }
 
+        userRepository.save(user);
+        return new ResponseEntity(HttpStatus.OK);
     }
-
 
 
 }
