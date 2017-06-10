37eac3232c7eeb87d56d15240c24720066512c05
Oskar Polak <oskarbank@gmail.com>
2017-06-10 13:29:19 +0200

    Security

--- src/main/java/com/example/demo/controllers/SecureController.java
+++ src/main/java/com/example/demo/controllers/SecureController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.UserInfoBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -9,6 +12,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class SecureController {
+
+    @Autowired
+    UserInfoBean userInfo;
+
+
+        @GetMapping("/user/{message}")
+        @ResponseBody
+        public String setUserInfo(@PathVariable("message") String message){
+            userInfo.setMessage(message);
+            return "Ustawiłem tekst dla Ciebie: " +  message;
+        }
+
+         @GetMapping("/user")
+         @ResponseBody
+         public String getUserInfo(){
+             return "Twój tekst to: " + userInfo.getMessage();
+         }
 
         @GetMapping("/") // to samo co @RequestMapping(value = "/", method = GET)
         public String main(){
